98a77cedaa2de0bf0816b882648145369de6991c
jqureshi <jqureshi@ggc.edu>
2022-09-06 22:02:08 -0400

    Created Firebase and added implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBXfLZxzGsXBd6nGlgqhScchooa1HurpZg",
+  authDomain: "scheme-51b6b.firebaseapp.com",
+  projectId: "scheme-51b6b",
+  storageBucket: "scheme-51b6b.appspot.com",
+  messagingSenderId: "275195110296",
+  appId: "1:275195110296:web:a2b028178406603a7741ed",
+  measurementId: "G-8PDKGCJNCS"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 
 function App() {
