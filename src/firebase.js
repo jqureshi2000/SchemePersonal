c5c2e32208ccfc94ac4ad26ae0d9660bec72a6c0
jqureshi <jqureshi@ggc.edu>
2022-09-06 22:29:55 -0400

    Created Firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+​​import {
+​​  GoogleAuthProvider,
+​​  getAuth,
+​​  signInWithPopup,
+​​  signInWithEmailAndPassword,
+​​  createUserWithEmailAndPassword,
+​​  sendPasswordResetEmail,
+​​  signOut,
+​​} from "firebase/auth";
+​​import {
+​​  getFirestore,
+​​  query,
+​​  getDocs,
+​​  collection,
+​​  where,
+​​  addDoc,
+​​} from "firebase/firestore";
